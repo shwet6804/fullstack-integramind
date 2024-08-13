54e31445ba056933a2ce1e793ace55d7f800828c
Shwet Kumar <111366487+shwet6804@users.noreply.github.com>
2024-08-13 16:51:47 +0530

    third commit

---
+++ JS/document.js
@@ -0,0 +1,26 @@
+document.write("External JavaScript")
+document.write("<br>")
+document.write("<h1>BRANCHES IN GLA")
+document.write("<br>")
+document.write("<br>")
+document.write("<h3>CSE")
+document.write("<br>")
+document.write("<h3>Mechanical<br>")
+document.write("<h3>Electrical<br>")
+document.write("<h3>Specialization courses<hr>")
+
+var Eid=1001
+let Ename="Jarvis"
+const Esal=200000
+document.write("Employee id is:"+Eid)
+document.write("<hr>")
+document.write("Employee name is:"+Ename)
+document.write("<hr>")
+document.write("Employee salary is:"+Esal)
+document.write("<hr>")
+document.write("<br>")
+var obj1=3000
+var obj2=5564
+var res=obj1+obj2
+document.write("your result is:"+res)
+console.log(res)
---
+++ JS/user.py
@@ -0,0 +1,4 @@
+str1=input("enter first no.")
+str2=input("enter decond no.")
+res=str1+str2
+print("your result is:"+res)
